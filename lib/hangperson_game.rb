7eb4a16ad5376cd11ad11bba491c2cbed3129fe9
Aaron Yuan <yuaaar@gmail.com>
2016-09-14 23:40:48 +0000

    Ready for Heroku!

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,9 +7,12 @@ class HangpersonGame
 
   # def initialize()
   # end
+  attr_accessor :word, :guesses, :wrong_guesses
   
   def initialize(word)
-    @word = word
+    @word = word.downcase
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
   def self.get_random_word
@@ -17,6 +20,35 @@ class HangpersonGame
     require 'net/http'
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
+  end
+
+  def guess(char)
+    if !(char =~ /^[[:alpha:]]$/)
+      raise ArgumentError
+    end
+    char = char.downcase
+    if (@guesses+@wrong_guesses).include?(char)
+      return false
+    elsif @word.include?(char)
+      @guesses += char
+    elsif !@word.include?(char)
+      @wrong_guesses += char
+    end
+    return true
+  end
+  
+  def word_with_guesses
+    @word.gsub(/[^#{'#'+@guesses}]/, '-')
+  end
+  
+  def check_win_or_lose
+    if @wrong_guesses.length >= 7
+      return :lose
+    elsif word_with_guesses.include?('-')
+      return :play
+    else
+      return :win
+    end
   end
 
 end
